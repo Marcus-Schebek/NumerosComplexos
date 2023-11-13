893ae58867c9d806ac627d06f023bde92ab27cac
Marcus-Schebek <43122453+Marcus-Schebek@users.noreply.github.com>
2023-11-13 08:32:14 -0300

    Melhorando o código em Python

--- Python/NumComplex.py
+++ Python/NumComplex.py
@@ -1,6 +1,3 @@
-from typing import Any
-
-
 class NumComplex:
     def __init__(self, real, imaginary):
         self.real = real
@@ -26,25 +23,25 @@ class NumComplex:
         newReal = (self.get_real() * other.get_real() + self.get_imaginary() * other.get_imaginary())/denominator
         newImaginary = (self.get_real() * other.get_real() - self.get_imaginary() * other.get_imaginary())/denominator
         return NumComplex(newReal, newImaginary)
-    def __str__(self) -> str: #Método mágico que permite definir uma representação em string personalizada para nossa classe
+    def __str__(self) -> str: #Método que permite definir uma representação em string personalizada para nossa classe, achei lindo 
         if self.get_imaginary() >= 0:
             return f"{self.get_real()} + {self.get_imaginary()}i"
         else:
             return f"{self.get_real()} - {-self.get_imaginary()}i"
 
-    # Leitura de números complexos
+    
 real1, imaginary1 = map(float, input("Digite o primeiro número complexo (parte real e parte imaginária separadas por espaço): ").split(" "))
 real2, imaginary2 = map(float, input("Digite o segundo número complexo (parte real e parte imaginária separadas por espaço): ").split(" "))
 num1 = NumComplex(real1, imaginary1)
 num2 = NumComplex(real2, imaginary2)
 
-# Realização de operações
+
 sum_result = num1.add(num2)
 difference_result = num1.subtract(num2)
 product_result = num1.multiply(num2)
 
 
-# Impressão dos resultados
+
 print("Resultado da Soma:", sum_result)
 print("Resultado da Subtração:", difference_result)
 print("Resultado da Multiplicação:", product_result)
